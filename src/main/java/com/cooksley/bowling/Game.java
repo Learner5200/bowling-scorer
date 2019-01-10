cd8b7dd9a684614e83a607478d1ee6193c6e7472
Henry Cooksley <henry.cooksley@gmail.com>
2019-01-10 15:30:44 +0000

    Add Game class, plus empty roll, score methods

---
+++ src/main/java/com/cooksley/bowling/Game.java
@@ -0,0 +1,11 @@
+package com.cooksley.bowling;
+
+public class Game {
+    public void roll(int value) {
+
+    }
+
+    public int calculateScore() {
+        return 0;
+    }
+}
--- src/test/FrameTest.java
+++
@@ -1,7 +0,0 @@
-package test;
-
-public class FrameTest {
-//    @Test
-//    public void
-
-}
