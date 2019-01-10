5d055f51e3af8a8198820fee2ea9f0ddb6e010c4
Chris Cooksley <christopher.cooksley@gmail.com>
2019-01-10 16:19:17 +0000

    Failing test for Frame.roll()

--- src/test/java/com/cooksley/bowling/FrameTest.java
+++ src/test/java/com/cooksley/bowling/FrameTest.java
@@ -1,12 +1,21 @@
 package com.cooksley.bowling;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class FrameTest {
+    Frame frame;
+
     @BeforeEach
     public void setup() {
-        Frame frame = new Frame();
+        frame = new Frame();
+    }
+
+    @Test
+    public void rollAddsItsValueToRolls() {
+        frame.roll(5);
+        Assertions.assertEquals(5, frame.rolls[0]);
     }
 
 }
