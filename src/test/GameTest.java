f65462bb69821e695c3f08fea9ad1855ceb2bb0a
Chris Cooksley <christopher.cooksley@gmail.com>
2019-01-10 15:15:09 +0000

    Test for gutter game failing

---
+++ src/test/GameTest.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+    Game game;
+
+    @BeforeEach
+    public void setup() {
+        game = new Game();
+    }
+
+    @Test
+    public void testGutterGame() {
+        for (int i = 0; i < 20; i++) game.roll(0);
+        Assertions.assertEquals(0, game.calculateScore());
+    }
+}
